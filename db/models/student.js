da3252e8edc95f77726cf25fc92af5a1542d7a26
Jason Li <iverefiner@gmail.com>
2020-03-07 19:15:30 -0600

    finished cycle-1

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,36 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+	firstName: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true
+		}
+	},
+	lastName: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true
+		}
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			isEmail: true,
+			notEmpty: true,
+			len: [ 1, 255 ]
+		}
+	}
+});
+
+Student.beforeCreate((studentInstance, optionsObject) => {
+	studentInstance.firstName =
+		studentInstance.firstName.slice(0, 1).toUpperCase() + studentInstance.firstName.slice(1);
+	studentInstance.lastName = studentInstance.lastName.slice(0, 1).toUpperCase() + studentInstance.lastName.slice(1);
+});
 
 module.exports = Student;
--- db/models/test.js
+++ db/models/test.js
@@ -3,6 +3,24 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+const Test = db.define('test', {
+	subject: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true
+		}
+	},
+	grade: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		validate: {
+			notEmpty: true
+		}
+	}
+});
+
+Test.belongsTo(Student);
+Student.hasMany(Test);
 
 module.exports = Test;
