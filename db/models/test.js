c177bc8206d85b5fdbb3ef148bfc29faf586ca18
Jason Li <iverefiner@gmail.com>
2020-03-07 19:25:18 -0600

    cleared stuff that was supposed to be for cycle-2

--- db/models/test.js
+++ db/models/test.js
@@ -3,24 +3,6 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define('test', {
-	subject: {
-		type: Sequelize.STRING,
-		allowNull: false,
-		validate: {
-			notEmpty: true
-		}
-	},
-	grade: {
-		type: Sequelize.INTEGER,
-		allowNull: false,
-		validate: {
-			notEmpty: true
-		}
-	}
-});
-
-Test.belongsTo(Student);
-Student.hasMany(Test);
+const Test = db.define();
 
 module.exports = Test;
